58a4547730e91bf41508b4684d8b940b4c5e471c
ssssssssad <95890805+ssssssssad@users.noreply.github.com>
2022-01-21 08:58:03 -0800

    Create AntiJail.lua

---
+++ AntiJail.lua
@@ -0,0 +1,7 @@
+local player = game.Players.LocalPlayer
+while true do
+	wait(1)
+if game:GetService("Workspace"):FindFirstChild("sdhhf1245_ADONISJAIL") then
+	game.Players:Chat(":unjail me")
+end
+end
